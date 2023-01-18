e592754e7e551eab09162208586ebe715aa2581e
highlyreactiveNeon <unravellingneon@outlook.com>
2023-01-18 18:59:05 +0530

    basic neovim configs

---
+++ lua/configs.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " "
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 100
+
+vim.opt.background = "dark"
+vim.g.gruvbox_contrast_dark = "medium"
+vim.cmd.colorscheme("gruvbox")
---
+++ lua/plugins.lua
@@ -0,0 +1,38 @@
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use 'vim-airline/vim-airline'
+    use 'morhetz/gruvbox'
+    use 'tpope/vim-unimpaired'
+    use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+    use 'nvim-treesitter/playground'
+    use 'mbbill/undotree'
+    use 'preservim/nerdtree'
+    use 'preservim/tagbar'
+    use 'ryanoasis/vim-devicons'
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
+end)
